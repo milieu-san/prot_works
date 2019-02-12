9eb67b35a7b5d8865ce95b7e5910e63291af396d
masahiro <aisacart@gmail.com>
2019-02-12 17:15:50 +0900

    add columns to users_table

---
+++ db/migrate/20190212081051_add_column_to_user.rb
@@ -0,0 +1,7 @@
+class AddColumnToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string, null: false
+    add_column :users, :nick_name, :string, null: false
+    add_column :users, :profile, :text, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_075414) do
+ActiveRecord::Schema.define(version: 2019_02_12_081051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 2019_02_12_075414) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "nick_name", null: false
+    t.text "profile", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
