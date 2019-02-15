50fc9123bc7ae443bf926c8c20189ba1460a3367
masahiro <aisacart@gmail.com>
2019-02-15 18:01:17 +0900

    add before action in preview/node/controller

--- app/controllers/preview/nodes_controller.rb
+++ app/controllers/preview/nodes_controller.rb
@@ -1,9 +1,15 @@
 class Preview::NodesController < ApplicationController
-
+  before_action :private_protect
   # GET /nodes
   # GET /nodes.json
   def index
     @prot = Prot.find(params[:prot_id])
     @nodes = @prot.nodes.all.order(position: :asc)
+  end
+
+  private
+  def private_protect
+    prot = Prot.find(params[:prot_id])
+    raise StandardError if prot.private == true && prot.user_id != current_user.id
   end
 end
