b287beae2ee2a6b39bd5ce8a043bd2fa77b96e77
masahiro <aisacart@gmail.com>
2019-02-15 13:59:22 +0900

    fail to update bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jstree.min
+//= require jquery3
 //= require bootstrap-sprockets
+//= require popper
+
+//= require jstree.min
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
