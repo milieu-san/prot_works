7cda8f069e3fa9c4582f26f057ae2d32e18fb599
masahiro <aisacart@gmail.com>
2019-02-17 18:14:54 +0900

    create create_action in comment_controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.new(comment_params)
+    if @comment.save
+      redirect_to prot_review_path(@comment.review.prot_id, @comment.review_id)
+      flash[:success] = "コメントを投稿しました"
+    else
+      redirect_to prot_review_path(Review.find(@comment.review_id).prot_id, @comment.review_id)
+      flash[:danger] = "コメントを投稿できませんでした"
+    end
+
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment)
+          .permit(:body,
+                  :review_id)
+  end
+end
