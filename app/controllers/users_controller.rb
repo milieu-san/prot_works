9be86a01bf0be883f6f785f6728a237db526b5d9
masahiro <aisacart@gmail.com>
2019-02-18 16:51:08 +0900

    create user/shoe page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,31 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def mypage
   end
 
-  def show
+  def edit
+    @user = User.find(current_user.id)
   end
 
-  def edit
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to mypage_path
+    else
+      render :edit
+    end
   end
 
   private
 
-  def set_user
+  def user_params
+    params.require(:user)
+          .permit(:name,
+                  :nick_name,
+                  :profile)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
   get 'homes/index'
   devise_for :users
+  devise_scope :user do
+    get 'profile_edit', to: 'users/registrations#profile_edit', as: 'profile_edit'
+    patch 'profile_update', to: 'users/registrations#profile_update', as: 'profile_update'
+  end
 
   unauthenticated do
      root to: 'homes#index'
   end
 
   authenticated do
-    root to: 'users#show'
+    root to: 'users#mypage'
   end
 
   get 'mypage', to: 'users#mypage'
   get 'mypage/edit', to: 'users#edit'
+  patch 'mypage/update', to: 'users#update'
+
   resources :users, only: [:show]
 
   resources :prots do
@@ -24,7 +30,6 @@ Rails.application.routes.draw do
   namespace :preview do
     resources :nodes, only: [:index]
   end
-
 
   if Rails.env.development?
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
