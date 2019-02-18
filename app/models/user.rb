cf340fbf1d1d2f354183ce900d6ba49fc2028ff3
masahiro <aisacart@gmail.com>
2019-02-18 11:53:38 +0900

    add validates on user/model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :nick_name, presence: true, uniqueness: true, length: { maximum: 30 }, format: { with: /\A[a-z0-9]+\z/i }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
