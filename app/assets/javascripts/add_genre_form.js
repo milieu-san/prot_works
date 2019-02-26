13852b8008b55b5250c0fdd13ddfd5c456869e0a
masahiro <aisacart@gmail.com>
2019-02-26 11:39:30 +0900

    fix indent when add genre_mediatype form in prot_form

--- app/assets/javascripts/add_genre_form.js
+++ app/assets/javascripts/add_genre_form.js
@@ -3,7 +3,7 @@ $(function() {
   $('#add_genre_form').on('click', function() {
     var input =
         '<div class="genre_form">'
-        +'<label for="genres_attributes_'+ i +'_name"></label> '
+        +'<label for="genres_attributes_'+ i +'_name"></label>'
         +'<input type="text" name="prot[genres_attributes]['+ i +'][name]" id="genres_attributes_'+ i +'_name">'
         +'</div>'
     $('#field_genre_form').append(input);
--- app/assets/javascripts/add_media_form.js
+++ app/assets/javascripts/add_media_form.js
@@ -3,7 +3,7 @@ $(function() {
   $('#add_media_form').on('click', function() {
     var input =
         '<div class="media_form">'
-        +'<label for="media_types_attributes_'+ i +'_name"></label> '
+        +'<label for="media_types_attributes_'+ i +'_name"></label>'
         +'<input type="text" name="prot[media_types_attributes]['+ i +'][name]" id="media_types_attributes_'+ i +'_name">'
         +'</div>'
     $('#field_media_form').append(input);
