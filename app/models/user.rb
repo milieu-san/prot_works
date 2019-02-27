7edb46153ad57c906188c9f37a269efcffde8475
masahiro <aisacart@gmail.com>
2019-02-27 11:15:08 +0900

    change views aroud devise, user from en to ja

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,5 +23,4 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
   validates :nick_name, presence: true, uniqueness: true, length: { maximum: 30 }, format: { with: /\A[a-z0-9]+\z/i }
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
 end
