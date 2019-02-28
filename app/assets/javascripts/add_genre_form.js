57ad00329783bcbde3c9428ebe2ecc49077bbe66
masahiro <aisacart@gmail.com>
2019-02-28 16:25:46 +0900

    fix indent. delete commentout in preview node

--- app/assets/javascripts/add_genre_form.js
+++ app/assets/javascripts/add_genre_form.js
@@ -1,12 +1,12 @@
 $(document).on('turbolinks:load', function() {
-    var i = $('.genre_form').length;
-    $('#add_genre_form').on('click', function() {
-      var input =
-          '<div class="genre_form">'
-          +'<label for="genres_attributes_'+ i +'_name"></label>'
-          +'<input type="text" name="prot[genres_attributes]['+ i +'][name]" id="genres_attributes_'+ i +'_name">'
-          +'</div>'
-      $('#field_genre_form').append(input);
-      i ++ ;
-    });
+  var i = $('.genre_form').length;
+  $('#add_genre_form').on('click', function() {
+    var input =
+        '<div class="genre_form">'
+        +'<label for="genres_attributes_'+ i +'_name"></label>'
+        +'<input type="text" name="prot[genres_attributes]['+ i +'][name]" id="genres_attributes_'+ i +'_name">'
+        +'</div>'
+    $('#field_genre_form').append(input);
+    i ++ ;
   });
+});
